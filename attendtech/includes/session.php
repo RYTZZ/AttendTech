8a2a390d8ace7b13b4b3c86a594992ad4f33c189
Rytzz Garcia Grieta <rytzz.grieta@sorsu.edu.ph>
2026-05-23 13:31:27 +0800

    Delete attendtech directory

--- attendtech/includes/session.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    ini_set('session.cookie_httponly', 1);
-    ini_set('session.use_strict_mode', 1);
-    session_start();
-}
--- attendtech/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/config/app.php';
-header('Location: ' . BASE_URL . '/admin/dashboard.php');
-exit;
